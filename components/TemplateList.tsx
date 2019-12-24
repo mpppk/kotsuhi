c1f24c4d59e1213366bfa775d83cd58d1662c12a
mpppk <niboshiporipori@gmail.com>
2019-12-25 01:15:56 +0900

    Add badge to TemplateList

--- components/TemplateList.tsx
+++ components/TemplateList.tsx
@@ -1,7 +1,10 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Badge from '@material-ui/core/Badge';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import NoteIcon from '@material-ui/icons/Note';
 import * as React from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -33,6 +36,9 @@ export default function TemplateList() {
           selected={selectedIndex === 0}
           onClick={event => handleListItemClick(event, 0)}
         >
+          <ListItemIcon>
+            <NoteIcon />
+          </ListItemIcon>
           <ListItemText primary="Template1" />
         </ListItem>
         <ListItem
@@ -40,6 +46,11 @@ export default function TemplateList() {
           selected={selectedIndex === 1}
           onClick={event => handleListItemClick(event, 1)}
         >
+          <ListItemIcon>
+            <Badge badgeContent={3} color="secondary">
+              <NoteIcon />
+            </Badge>
+          </ListItemIcon>
           <ListItemText primary="Template2" />
         </ListItem>
         <ListItem
@@ -47,6 +58,11 @@ export default function TemplateList() {
           selected={selectedIndex === 2}
           onClick={event => handleListItemClick(event, 2)}
         >
+          <ListItemIcon>
+            <Badge badgeContent={2} color="secondary">
+              <NoteIcon />
+            </Badge>
+          </ListItemIcon>
           <ListItemText primary="Template3" />
         </ListItem>
       </List>
